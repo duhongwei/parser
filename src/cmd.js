6077e401ddc81cab619904ccc1d1e12fc76494c0
duhongwei <757318006@qq.com>
2019-02-25 16:11:54 +0800

    2.1.0

--- src/cmd.js
+++ src/cmd.js
@@ -1,20 +1,32 @@
 /**
  * parse cmd module
+ * 
+ * 先在代码开始加上 var  exports={}
+ * 然后在把module.exports中的 moudle. 删除
+ * 导出exports 为default
  *
  */
 const acorn = require('acorn')
 const walk = require('acorn-walk')
-
+const debug = require('debug')('parser/cmd')
 module.exports = class {
   constructor(code) {
     this.ast = acorn.parse(code)
+    debug(JSON.stringify(this.ast, null, 2))
     this.code = code
     this.delPosition = []
   }
   parse() {
+    //检查这个脚本是否为引导脚本。
+    let realFile = this._checkIf()
+    if (realFile) {
+      return {
+        link: realFile
+      }
+    }
     const importInfo = this._parseImport()
     let exportInfo = this._parseExport()
-  
+
     let code = this._delCode()
     code = `var exports={};${code}`
     return {
@@ -24,6 +36,44 @@ module.exports = class {
     }
   }
   //================= private ====================
+  _testIf(testNode) {
+    let code = `${testNode.left.raw}${testNode.operator}${testNode.right.raw}`
+    debug(`test code: ${code}`)
+    return eval(code)
+  }
+  _checkIf() {
+    const that = this
+    let result = false
+    walk.simple(this.ast, {
+      IfStatement(node) {
+        if (!node.test.left || !node.test.right) {
+          return
+        }
+        if (node.test.left.value === 'development' || node.test.left.value === 'production') {
+          let requireNode = null
+
+          if (that._testIf(node.test)) {
+            requireNode = node.consequent
+
+          }
+          else {
+            requireNode = node.alternate
+
+          }
+          if (requireNode) {
+            walk.simple(requireNode, {
+              CallExpression(node) {
+                if (node.callee.name === 'require') {
+                  result = node.arguments[0].value
+                }
+              }
+            })
+          }
+        }
+      }
+    })
+    return result
+  }
   _delCode() {
     let code = ''
     let start = 0
@@ -47,7 +97,7 @@ module.exports = class {
       }
 
     }
-
+    this.delPosition = []
     code += this.code.slice(start)
     return code
 
@@ -55,8 +105,33 @@ module.exports = class {
   _parseImport() {
     var importInfo = []
     var that = this
-    walk.simple(this.ast, {
-      VariableDeclaration(node) {
+    walk.ancestor(this.ast, {
+      CallExpression(node, ancestors) {
+        if (node.callee.name !== 'require') {
+          return
+        }
+        //取到直接上级
+        let p = ancestors[ancestors.length - 2]
+        //取到上级的上级
+        let pp = ancestors[ancestors.length - 3]
+        if (p.type === 'VariableDeclarator') {
+          let { token, file } = that._parseDeclarations(pp.declarations)
+          that.delPosition.push([pp.start, pp.end])
+          if (!file) {
+            return
+          }
+          importInfo.push({
+            type: 'cjs',
+            file,
+            token
+          })
+        }
+        if (p.type == 'AssignmentExpression') {
+          throw new Error('暂时不能解析赋值，只能解析声明变量')
+        }
+      }
+
+      /* VariableDeclaration(node) {
         let { token, file } = that._parseDeclarations(node.declarations)
         if (!file) {
           return
@@ -67,23 +142,23 @@ module.exports = class {
           token
         })
         that.delPosition.push([node.start, node.end])
-      }
+      } */
+
     })
     return importInfo
   }
   _parseExport() {
     const that = this
     walk.simple(this.ast, {
-      ExpressionStatement(node) {
+      MemberExpression(node) {
 
-        if (node.expression.type === 'AssignmentExpression' && node.expression.left.type === 'MemberExpression' && node.expression.left.object && node.expression.left.object.type == 'Identifier') {
-          const name = node.expression.left.object.name
-          if (name === 'module') {
-            let delStart = node.expression.left.object.start
-            let delEnd = node.expression.left.object.end + 1
-            that.delPosition.push([delStart, delEnd])
-          }
+        if (node.object.name != 'module') {
+          return
         }
+        if (node.property.name != 'exports') {
+          return
+        }
+        that.delPosition.push([node.object.start, node.object.end + 1])
       }
     })
     return [{
@@ -94,30 +169,23 @@ module.exports = class {
   _parseDeclarations(declarations) {
     let token = []
     let file = null
-    for (const item of declarations) {
-
-      if (item.type === 'VariableDeclarator') {
-        if (item.init.type === 'CallExpression' && item.init.callee.type == 'Identifier' && item.init.callee.name === 'require') {
-
-          if (declarations.length > 1) {
-            throw new Error('do not support multiple declaration in one statement');
-          }
-          file = item.init.arguments[0].value
-          if (item.id.type === 'Identifier') {
-            token.push({
-              from: 'default',
-              to: item.id.name
-            })
-          }
-          if (item.id.type === 'ObjectPattern') {
-            for (let p of item.id.properties) {
-              token.push({
-                from: p.key.name,
-                to: p.key.name
-              })
-            }
-          }
-        }
+    if (declarations.length > 1) {
+      throw new Error('do not support multiple declaration in one statement');
+    }
+    let item = declarations[0]
+    file = item.init.arguments[0].value
+    if (item.id.type === 'Identifier') {
+      token.push({
+        from: 'default',
+        to: item.id.name
+      })
+    }
+    if (item.id.type === 'ObjectPattern') {
+      for (let p of item.id.properties) {
+        token.push({
+          from: p.key.name,
+          to: p.key.name
+        })
       }
     }
     return {
--- test/cmd.js
+++ test/cmd.js
@@ -1,6 +1,17 @@
 const { Cmd } = require('../index.js')
 const assert = require('assert')
 describe('parse common js', () => {
+  describe('not suport', () => {
+    it('require in assign expression', () => {
+      let code = `
+      module.exports = require('./lib/axios');`
+      const fromCmd = new Cmd(code)
+      let fun = () => {
+        fromCmd.parse()
+      }
+      assert.throws(fun)
+    })
+  })
   describe('parse require', () => {
     it('require a from "a.js"', () => {
       let code = "const a = require('a.js')"
@@ -26,6 +37,14 @@ describe('parse common js', () => {
       assert.deepEqual(exportInfo, toExportInfo)
       assert.equal(code, toCode)
     })
+    it('inset module.exports', () => {
+      const fromCmd = new Cmd("{let a;a=1, module.exports={a:1} }")
+      let { exportInfo, code } = fromCmd.parse()
+      let toExportInfo = [{ from: 'exports', to: 'default' }]
+      let toCode = 'var exports={};{let a;a=1, exports={a:1} }'
+      assert.deepEqual(exportInfo, toExportInfo)
+      assert.equal(code, toCode)
+    })
     it('exports', () => {
       const fromCmd = new Cmd("exports.a=1")
       let { exportInfo, code } = fromCmd.parse()
@@ -34,5 +53,26 @@ describe('parse common js', () => {
       assert.deepEqual(exportInfo, toExportInfo)
       assert.equal(code, toCode)
     })
+   
+  })
+  describe('parse if require', () => {
+    it('if exports', () => {
+      const fromCmd = new Cmd(`
+      const a=require('./a')
+      var exports={};
+      if ("development" === 'production') {
+        exports = require('./vue.runtime.common.prod.js')
+      } else {
+        exports = require('./vue.runtime.common.dev.js')
+      }
+            
+      `)
+      let { link } = fromCmd.parse()
+
+      let toLink = './vue.runtime.common.dev.js'
+      assert.equal(link, toLink)
+
+    })
+
   })
 })
